d8036ed370c12e42143d215d15537658f934e2a0
JuanF2905 <Hackerjoan2529@gmail.com>
2025-06-28 20:50:49 -0400

    Delete Para nadie directory

--- Para nadie/Script.js
+++
@@ -1,8 +0,0 @@
-// Seleccionamos el elemento de la tarjeta
-const card = document.querySelector('.card');
-
-// Añadimos un "escuchador de eventos" para el clic
-card.addEventListener('click', function() {
-    // Cada vez que se hace clic, se añade o quita la clase 'is-flipped'
-    card.classList.toggle('is-flipped');
-});
